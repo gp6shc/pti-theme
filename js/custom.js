d75f00197db9445b6109322814ec18f22ef0cb4c
B. O'Toole <botoole518@gmail.com>
2015-01-12 12:22:17 -0500

    back to top

--- js/custom.js
+++ js/custom.js
@@ -57,23 +57,31 @@ custom events
 	 /* ----------------------------------------------------------- */
 	 /*  Back to top
 	 /* ----------------------------------------------------------- */
-
-			 $(window).scroll(function () {
-						if ($(this).scrollTop() > 50) {
-								$('#back-to-top').fadeIn();
-						} else {
-								$('#back-to-top').fadeOut();
-						}
-				});
-			// scroll body to 0px on click
-			$('#back-to-top').click(function () {
-					$('#back-to-top').tooltip('hide');
-					$('body,html').animate({
-							scrollTop: 0
-					}, 800);
-					return false;
-			});
-			
-			$('#back-to-top').tooltip('hide');
-
+		$(window).on('scroll', function(){
+		  
+		  var wHeight         = $(window).height(),
+		      distanceFromTop = $(window).scrollTop(),
+		      bodyHeight      = $('body,html').height(),
+		      bodyMinusWindow = bodyHeight - (wHeight + 200);
+		  
+		  if (distanceFromTop > bodyMinusWindow) {
+		    $('#top').addClass('active');
+		  } else {
+		    $('#top').removeClass('active');
+		  }
+		   
+		});
+		
+		$('#top').click(function(e){
+		  
+		  e.preventDefault();
+		  
+		  $('body,html').animate({
+		    'scrollTop': 0
+		  }, 700);
+		  
+		});
+		
+		
+//close
 });

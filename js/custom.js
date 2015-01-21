1d3d5deabfac4b68ee654a470b121f0beeada745
Mike <mike@sachsmedia.com>
2015-01-21 09:47:29 -0500

    minor style tweaks to better match design specs

--- js/custom.js
+++ js/custom.js
@@ -166,7 +166,8 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 	$('#transition-timer-carousel-opaque, #transition-timer-carousel, #transition-timer-carousel-two').carousel({
-		interval: 4000
+		interval: 4000,
+		pause: ""
 	});
 });
 	

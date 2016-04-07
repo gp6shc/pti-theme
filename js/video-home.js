93958e6173b5b505bbf870a4e34323fc9a28b3b2
brianotoole <botoole518@gmail.com>
2016-04-07 18:43:59 -0400

    homepg video client edits

--- js/video-home.js
+++ js/video-home.js
@@ -14,14 +14,16 @@
           playerVars: {
               'autoplay': 1,
               'loop': 1,
-              'playlist': 'b_NefZMJySs',
+              'html5': 1, //force html5 vs flash... may need onError() fallback 
+              'playlist': 'nXpU7GX5Id0', //required for looping :(
               'controls': 0,
               'rel': 0,
               'enablejsapi': 1,
               'showinfo': 0,
-              'autohide': 0,
+              'autohide': 1,
               'modestbranding': 1,
                   vq: 'hd1080'},
+                  videoId: 'nXpU7GX5Id0',
           events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
@@ -33,38 +35,30 @@
       function onPlayerReady(event) {
         event.target.playVideo();
         player.mute();
+
       }
 
       var done = false;
       function onPlayerStateChange(event) {
-        
+
       }
-      function stopVideo() {
-        player.stopVideo();
-      }
-    
-      
+
 // When play button is clicked
 $("#play-video").click(function () {
-    $(".title").hide(); //hide text, but keep overlay
-    player.pauseVideo(); //pause video
-    
-});
-
-// On modal open
-$('#play-full').on('shown.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // button that triggered the modal
-    //var videoSource = button.data('video-source') + "?rel=0&showinfo=0&autoplay=1&autohide=1";
+  $(".title").hide(); //hide text, but keep overlay
+  player.pauseVideo(); //pause video
+    var button = $(event.relatedTarget); //button that triggered the modal
     var timeElapsed = player.getCurrentTime();
-    var timeTruncated = Math.round(timeElapsed);
-    var videoSource = "//www.youtube.com/embed/b_NefZMJySs" + "?rel=0&showinfo=0&autoplay=1&autohide=1" + "&start=" + timeTruncated;
-    $(button).attr("data-video-source", videoSource);
-    $('#home-iframe').attr("src", videoSource);   
+    var timeTruncated = Math.round(timeElapsed); //round num to 1 integer
+    var videoSource = "https://www.youtube.com/embed/nXpU7GX5Id0" + "?rel=0&showinfo=0&autoplay=1&autohide=1" + "&start=" + timeTruncated;
+    $(button).prop("data-video-source", videoSource); //attach data-video-source to modal button
+    $('#home-iframe').prop("src", videoSource); //attach src to modal iframe 
 });
 
 // On modal close
-$('#play-full').on('hide.bs.modal', function () { 
-    $('#home-iframe').attr("src", "");  
-    $(".title").show(); //show text 
-    player.playVideo(); //resume video
+$('#play-full').click(function(e) {
+  e.stopPropagation();
+  $('#home-iframe').prop("src", ""); //remove 
+  $(".title").show(); //show text 
+  player.playVideo(); //resume video
 });
